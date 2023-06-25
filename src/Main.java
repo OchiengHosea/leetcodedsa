d06b426d4a3e672b5ed78a32427ce53f42a73d14
OchiengHosea <jrhosea8@gmail.com>
2023-06-25 14:56:21 +0300

    merge sorted array

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import arrays.InsertingItemsIntoAnArray;
 
 public class Main {
     public static void main(String[] args) {
-        InsertingItemsIntoAnArray.duplicateZeros(new int[]{1,0,0,2,3,0,0,4,5,0,0});
+//        InsertingItemsIntoAnArray.duplicateZeros(new int[]{1,0,0,2,3,0,0,4,5,0,0});
+        int[] arr1 = new int[] {1,2,3,0,0,0};
+        int[] arr2 = new int[] {2,5,6};
+        InsertingItemsIntoAnArray.mergeSortedArray(arr1, arr1.length, arr2, arr2.length);
+        for (int n :
+                arr1) {
+            System.out.println(n);
+        }
     }
 }
--- src/arrays/InsertingItemsIntoAnArray.java
+++ src/arrays/InsertingItemsIntoAnArray.java
@@ -95,4 +95,12 @@ public class InsertingItemsIntoAnArray {
         }
 
     }
+
+    public static void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
 }
